4299fbb0e0f51aed6cba1333b17c723ac1dcc72c
uederstudy <uedersilva@live.com>
2024-07-15 22:44:30 -0300

    Update schemas.py

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -7,11 +7,11 @@ from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 
 class Atleta(BaseSchema):
-    nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
-    cpf: Annotated[str, Field(description='CPF do atleta', example='12345678900', max_length=11)]
-    idade: Annotated[int, Field(description='Idade do atleta', example=25)]
-    peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=75.5)]
-    altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.70)]
+    nome: Annotated[str, Field(description='Nome do atleta', example='Ueder', max_length=50)]
+    cpf: Annotated[str, Field(description='CPF do atleta', example='15054357158', max_length=11)]
+    idade: Annotated[int, Field(description='Idade do atleta', example=35)]
+    peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=80.0)]
+    altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.76)]
     sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
     categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
     centro_treinamento: Annotated[CentroTreinamentoAtleta, Field(description='Centro de treinamento do atleta')]
@@ -25,5 +25,5 @@ class AtletaOut(Atleta, OutMixin):
     pass
 
 class AtletaUpdate(BaseSchema):
-    nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
-    idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
+    nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Ueder Silva', max_length=50)]
+    idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=36)]
